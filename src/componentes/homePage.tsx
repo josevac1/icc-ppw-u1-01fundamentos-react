dcb142c878495ff09385819c6b7f1cbbdd2923ff
Jose Vanegas <josevanp05@gmail.com>
2025-11-04 18:06:20 -0500

    react 2

--- src/componentes/homePage.tsx
+++ src/componentes/homePage.tsx
@@ -1,15 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function HomePage() {
   const [count, setCount] = useState(0);
+  const [autoCount, setAutoCount] = useState(0);
 
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAutoCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); 
+
+
+   // funciones que afectan a ambos
+  const handleSumar = () => {
+    setCount(count + 1);
+    setAutoCount(autoCount + 1);
+  };
+
+  const handleRestar = () => {
+    setCount(count - 1);
+    setAutoCount(autoCount - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setAutoCount(0);
+  };
   return (
     <div style={{ textAlign: "left" }}>
       <h2>Contador: {count}</h2>
-
-      <button onClick={() => setCount(count + 1)}>suma</button>
-      <button onClick={() => setCount(count - 1)}>resta</button>
-      <button onClick={() => setCount(0)}>Reiniciar</button>
+      <h2>Contador Automático: {autoCount}</h2>
+        <button onClick={handleSumar}>Suma</button>
+        <button onClick={handleRestar}>Resta</button>
+        <button onClick={handleReset}>Reiniciar</button>
     </div>
+
+
+
     );
 } 
