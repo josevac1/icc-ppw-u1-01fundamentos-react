6bf8a25b56fcf04d2df2b377576e30ad2de3448a
Jose Vanegas <josevanp05@gmail.com>
2025-11-04 16:52:59 -0500

    reat6

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import HomePage from './componentes/homePage';
-import PerfilHome from './componentes/perfilHome';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import HomePage from "./componentes/homePage";
+import PerfilHome from "./componentes/perfilHome";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/01-fundamentos-react">
       <h1>Mi Aplicación</h1>
       <Routes>
-        <Route path="" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/perfil" element={<PerfilHome />} />
       </Routes>
     </BrowserRouter>
